7334183a022e3743db7b5d54f58f9ac1776f2dc7
Alexandre Bintz <alexandre@bintz.io>
2021-08-21 21:01:03 +0200

    write to file

--- SVGGenerator/main.swift
+++ SVGGenerator/main.swift
@@ -3,4 +3,15 @@ import Foundation
 
 
 
-print("Hello, World!")
+let outputFileURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent("generated.svg")
+
+let svgContentString = "test2"
+
+func write(_ content: String, to url: URL) {
+
+    try! svgContentString.data(using: .utf8)!.write(to: outputFileURL)
+}
+
+write(svgContentString, to: outputFileURL)
+
+print(outputFileURL)
