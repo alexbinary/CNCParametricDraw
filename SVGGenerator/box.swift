db5aed71a8454527826a58aa18ef2cffe749c778
Alexandre Bintz <alexandre@bintz.io>
2021-08-30 18:00:40 +0200

    BoxFace

--- SVGGenerator/box.swift
+++ SVGGenerator/box.swift
@@ -3,53 +3,120 @@ import Foundation
 
 
 
+enum BoxCrenelDirection {
+    
+    case external
+    case `internal`
+}
+
+
+struct BoxCrenelConfig {
+    
+    var direction: BoxCrenelDirection
+    var crenelConfig: CrenelConfig
+    var numberOfCrenels: UInt
+}
+
+
 struct BoxFace: PathProtocol {
         
     let size: Size
     
-    let leftCrenelConfig: CrenelConfig?
-    let rightCrenelConfig: CrenelConfig?
-    let topCrenelConfig: CrenelConfig?
-    let bottomCrenelConfig: CrenelConfig?
+    let leftCrenelConfig: BoxCrenelConfig?
+    let rightCrenelConfig: BoxCrenelConfig?
+    let topCrenelConfig: BoxCrenelConfig?
+    let bottomCrenelConfig: BoxCrenelConfig?
     
     var commands: [PathCommand] {
         
         var commands: [PathCommand] = []
         
-        commands.append(.moveToRelative(Coordinate(x: leftCrenelConfig?.depth ?? 0, y: topCrenelConfig?.depth ?? 0)))
+        let offsetLeft = leftCrenelConfig != nil && leftCrenelConfig!.direction == .external ? leftCrenelConfig!.crenelConfig.depth : 0
+        let offsetRight = rightCrenelConfig != nil && rightCrenelConfig!.direction == .external ? rightCrenelConfig!.crenelConfig.depth : 0
         
-        let totalLengthVertical = size.height - (topCrenelConfig?.depth ?? 0) - (bottomCrenelConfig?.depth ?? 0)
-        let totalLengthHorizontal = size.width - (leftCrenelConfig?.depth ?? 0) - (rightCrenelConfig?.depth ?? 0)
+        let offsetTop = topCrenelConfig != nil && topCrenelConfig!.direction == .external ? topCrenelConfig!.crenelConfig.depth : 0
+        let offsetBottom = bottomCrenelConfig != nil && bottomCrenelConfig!.direction == .external ? bottomCrenelConfig!.crenelConfig.depth : 0
         
-        if let leftCrenelConfig = leftCrenelConfig {
-            commands.append(contentsOf:
-                                CrenelPath(
-                                    totalLength: totalLengthVertical,
-                                    numberOfCrenels: 5,
-                                    crenelConfig: leftCrenelConfig,
-                                    offsetStart: 0,
-                                    offsetEnd: 0
-                                ).commands
-            )
+        commands.append(.moveToRelative(Coordinate(x: offsetLeft, y: offsetTop)))
+        
+        let horizontalLength = size.width - offsetLeft - offsetRight
+        let verticalLength = size.height - offsetTop - offsetBottom
+        
+        if let crenelConfig = leftCrenelConfig {
+            
+            var path = CrenelPath(
+                totalLength: size.height,
+                numberOfCrenels: crenelConfig.numberOfCrenels,
+                crenelConfig: crenelConfig.crenelConfig,
+                offsetStart: offsetTop,
+                offsetEnd: -offsetBottom
+            ).rotated90DegreesClockWise
+            
+            if crenelConfig.direction == .internal {
+                path = path.mirrorX
+            }
+            
+            commands.append(contentsOf:path.commands)
         } else {
-            commands.append(.lineToRelative(Coordinate(x: 0, y: totalLengthVertical)))
+            commands.append(.lineToRelative(Coordinate(x: 0, y: verticalLength)))
         }
         
-        if let bottomCrenelConfig = bottomCrenelConfig {
-            commands.append(contentsOf:
-                                CrenelPath(
-                                    totalLength: size.width,
-                                    numberOfCrenels: 5,
-                                    crenelConfig: bottomCrenelConfig,
-                                    offsetStart: leftCrenelConfig?.depth ?? 0,
-                                    offsetEnd: -(rightCrenelConfig?.depth ?? 0)
-                                ).commands
+        if let crenelConfig = bottomCrenelConfig {
+            
+            var path: PathProtocol = CrenelPath(
+                totalLength: size.width,
+                numberOfCrenels: crenelConfig.numberOfCrenels,
+                crenelConfig: crenelConfig.crenelConfig,
+                offsetStart: offsetLeft,
+                offsetEnd: -offsetRight
             )
+            
+            if crenelConfig.direction == .internal {
+                path = path.mirrorY
+            }
+            
+            commands.append(contentsOf: path.commands)
         } else {
-            commands.append(.lineToRelative(Coordinate(x: totalLengthHorizontal, y: 0)))
+            commands.append(.lineToRelative(Coordinate(x: horizontalLength, y: 0)))
         }
         
-        // TODO continue
+        if let crenelConfig = rightCrenelConfig {
+            
+            var path = CrenelPath(
+                totalLength: size.height,
+                numberOfCrenels: crenelConfig.numberOfCrenels,
+                crenelConfig: crenelConfig.crenelConfig,
+                offsetStart: offsetBottom,
+                offsetEnd: -offsetTop
+            ).rotated270DegreesClockWise
+            
+            if crenelConfig.direction == .internal {
+                path = path.mirrorX
+            }
+            
+            commands.append(contentsOf:path.commands)
+        } else {
+            commands.append(.lineToRelative(Coordinate(x: 0, y: verticalLength)))
+        }
+        
+        if let crenelConfig = topCrenelConfig {
+            
+            var path: PathProtocol = CrenelPath(
+                totalLength: size.width,
+                numberOfCrenels: crenelConfig.numberOfCrenels,
+                crenelConfig: crenelConfig.crenelConfig,
+                offsetStart: offsetRight,
+                offsetEnd: -offsetLeft
+            ).rotated180DegreesClockWise
+            
+            if crenelConfig.direction == .internal {
+                path = path.mirrorY
+            }
+            
+            commands.append(contentsOf: path.commands)
+        } else {
+            commands.append(.lineToRelative(Coordinate(x: -horizontalLength, y: 0)))
+        }
         
         return commands
     }
--- SVGGenerator/main.swift
+++ SVGGenerator/main.swift
@@ -13,61 +13,50 @@ func write(_ content: String, to url: URL) {
 }
 
 
-let offsetStart: Float = 5
-let offsetEnd: Float = 5
-
-let path = Path(withCommands: [
-    .lineToRelative(Coordinate(x: 30, y: 0)),
-    .lineToRelative(Coordinate(x: -15, y: 15)),
-])
-
-let abstractSVGPaths = [
-    path.svgPath,
-//    path.flipped.svgPath,
-//    path.mirrorX.svgPath,
-//    path.mirrorY.svgPath,
-    path.rotated90DegreesClockWise.svgPath,
-//    SVGPath(withCommands: [
-//        .moveTo(SVGCoordinate(x: offsetStart, y: 0), .relative)
-//    ]).appending(CrenelPath(
-//        totalLength: 100,
-//        numberOfCrenels: 2,
-//        crenelConfig: CrenelConfig(
-//            baseLength: 2,
-//            lengthAdjustment: 0,
-//            depth: 1
-//        ),
-//        direction: .normal,
-//        offsetStart: offsetStart,
-//        offsetEnd: offsetEnd
-//    )),
-//    CrenelPath(
-//        totalLength: 100,
-//        numberOfCrenels: 5,
-//        crenelConfig: CrenelConfig(
-//            baseLength: 5,
-//            lengthAdjustment: 0,
-//            depth: 3.15
-//        ),
-//        direction: .normal,
-//        offsetStart: 0,
-//        offsetEnd: 0
-//    ).svgPath
-]
 
 let pathStyle = "fill:none;stroke:#000000;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
 
 let nodes = [
     SVGPathNode(
-        path: abstractSVGPaths[0].starting(at: SVGCoordinate(x: 0, y: 0)),
+        path: BoxFace(
+            size: Size(width: 100, height: 100),
+            leftCrenelConfig: BoxCrenelConfig(
+                direction: .external,
+                crenelConfig: CrenelConfig(
+                    baseLength: 5,
+                    lengthAdjustment: 0,
+                    depth: 3
+                ),
+                numberOfCrenels: 5
+            ),
+            rightCrenelConfig: BoxCrenelConfig(
+                direction: .internal,
+                crenelConfig: CrenelConfig(
+                    baseLength: 5,
+                    lengthAdjustment: 0,
+                    depth: 3
+                ),
+                numberOfCrenels: 5
+            ),
+            topCrenelConfig: nil,
+            bottomCrenelConfig: BoxCrenelConfig(
+                direction: .external,
+                crenelConfig: CrenelConfig(
+                    baseLength: 5,
+                    lengthAdjustment: 0,
+                    depth: 3
+                ),
+                numberOfCrenels: 5
+            )
+        ).svgPath,
         pathStyle: "fill:none;stroke:#000000;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1",
         nodeId: "n1"
     ),
-    SVGPathNode(
-        path: abstractSVGPaths[1].starting(at: SVGCoordinate(x: 0, y: 0)),
-        pathStyle: "fill:none;stroke:#0000ff;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1",
-        nodeId: "cren1"
-    ),
+//    SVGPathNode(
+//        path: abstractSVGPaths[1].starting(at: SVGCoordinate(x: 0, y: 0)),
+//        pathStyle: "fill:none;stroke:#0000ff;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1",
+//        nodeId: "cren1"
+//    ),
 //    SVGPathNode(
 //        path: abstractSVGPaths[2].starting(at: SVGCoordinate(x: 0, y: 0)),
 //        pathStyle: "fill:none;stroke:#00ff00;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1",
