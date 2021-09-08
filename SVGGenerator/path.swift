c02233473a5fe8197a52ad45bf9599f8ac4ac615
Alexandre Bintz <alexandre@bintz.io>
2021-09-08 20:16:45 +0200

    Move PathCommand into own file

--- SVGGenerator/path.swift
+++ SVGGenerator/path.swift
@@ -3,27 +3,6 @@ import Foundation
 
 
 
-/// The elementary component of a path.
-///
-enum PathCommand {
-
-   
-    /// Instructs a move of the drawing head without actually drawing something by the given X and Y components relative to the current position.
-    ///
-    case moveBy(Offset)
-    
-    
-    /// Instructs the drawing of the a line between the current position and the current position offset by the given X and Y components.
-    ///
-    case lineByMovingBy(Offset)
-    
-    
-    /// Instructs to close the path, i.e. draw a line from the current position to the starting point of the first line in the path.
-    ///
-    case close
-}
-
-
 
 struct Path {
     
