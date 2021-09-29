f9702f93cdb12943e5f953193ed22898f51b42a8
Alexandre Bintz <alexandre@bintz.io>
2021-09-29 15:20:48 +0200

    move LegoCrenelBox into own file

--- SVGGenerator/Shapes/LegoCrenelBox.swift
+++ SVGGenerator/Shapes/LegoCrenelBox.swift
@@ -3,13 +3,10 @@ import Foundation
 
 
 
-struct LegoCrenelBoxCase: PathsLayoutRepresentable {
+struct LegoCrenelBox: PathsLayoutRepresentable {
     
     
     let legoUnitLength: MetricLength
-    
-    let materialThickness: MetricLength
-    let margin: MetricLength
     
     let width: LegoLength
     let length: LegoLength
@@ -18,23 +15,18 @@ struct LegoCrenelBoxCase: PathsLayoutRepresentable {
     let crenelConfigInternal: CrenelConfig
     let crenelConfigExternal: CrenelConfig
     
-    let numberOfCrenelsWidth: NumerOfCrenelsEnum = .auto
-    let numberOfCrenelsLength: NumerOfCrenelsEnum = .auto
-    let numberOfCrenelsHeight: NumerOfCrenelsEnum = .auto
-    
-    
-    var actualWidth: MetricLength { width.resolveToMetric(using: legoUnitLength) + 2 * materialThickness + margin }
-    var actualLength: MetricLength { length.resolveToMetric(using: legoUnitLength) + 2 * materialThickness + margin }
-    var actualHeight: MetricLength { height.resolveToMetric(using: legoUnitLength) + materialThickness + margin }
+    var numberOfCrenelsWidth: NumerOfCrenelsEnum = .auto
+    var numberOfCrenelsLength: NumerOfCrenelsEnum = .auto
+    var numberOfCrenelsHeight: NumerOfCrenelsEnum = .auto
     
     
     var pathsLayout: PathsLayout {
         
         let box = CrenelBox(
             
-            width: actualWidth,
-            length: actualLength,
-            height: actualHeight,
+            width: width.resolveToMetric(using: legoUnitLength),
+            length: length.resolveToMetric(using: legoUnitLength),
+            height: height.resolveToMetric(using: legoUnitLength),
             
             widthCrenelConfig: BoxCrenelConfig(crenelConfigInternal: crenelConfigInternal, crenelConfigExternal: crenelConfigExternal, numberOfCrenels: numberOfCrenelsWidth),
             lengthCrenelConfig: BoxCrenelConfig(crenelConfigInternal: crenelConfigInternal, crenelConfigExternal: crenelConfigExternal, numberOfCrenels: numberOfCrenelsLength),
