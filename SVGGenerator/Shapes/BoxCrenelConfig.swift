bb152d3123cfdbf246578d4b3faaa660c946dbb6
Alexandre Bintz <alexandre@bintz.io>
2021-09-29 15:19:33 +0200

    move BoxCrenelConfig into own file

---
+++ SVGGenerator/Shapes/BoxCrenelConfig.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+
+
+struct BoxCrenelConfig {
+
+    
+    var crenelConfigInternal: CrenelConfig
+    var crenelConfigExternal: CrenelConfig
+    var numberOfCrenels: NumerOfCrenelsEnum
+}
--- SVGGenerator/Shapes/box.swift
+++ SVGGenerator/Shapes/box.swift
@@ -3,16 +3,6 @@ import Foundation
 
 
 
-struct BoxCrenelConfig {
-
-    
-    var crenelConfigInternal: CrenelConfig
-    var crenelConfigExternal: CrenelConfig
-    var numberOfCrenels: NumerOfCrenelsEnum
-}
-
-
-
 struct CrenelBox: PathsLayoutRepresentable {
     
     
