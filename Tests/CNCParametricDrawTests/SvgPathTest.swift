faab6759982f751442a63e1c80b8e1998c96e52d
Alexandre Bintz <alexandre@bintz.io>
2021-10-12 20:47:31 +0200

    SvgPath.optimize: make tests more unit

--- Tests/CNCParametricDrawTests/SvgPathTest.swift
+++ Tests/CNCParametricDrawTests/SvgPathTest.swift
@@ -44,48 +44,120 @@ class SvgPathTest: XCTestCase {
     }
     
     
-    /// Tests that the `.optimized` property removes a `.moveTo` command when it moves to relative (0,0), which is a noop.
+    /// Tests that the `.optimized` property does not change a `.moveTo` command whose target is not `(0,0)`
     ///
-    func test_optimized_move() {
+    func test_optimized_moveTo_nonZero() {
         
         let path = SvgPath(withCommands: [
-            .moveTo(Coordinates(x: 0, y: 0), .absolute),
-            .moveTo(Coordinates(x: 0, y: 0), .relative),
             .moveTo(Coordinates(x: 1, y: 2), .absolute),
             .moveTo(Coordinates(x: 1, y: 2), .relative),
         ])
         
         XCTAssertEqual(path.optimized.commands, [
-            .moveTo(Coordinates(x: 0, y: 0), .absolute),
             .moveTo(Coordinates(x: 1, y: 2), .absolute),
             .moveTo(Coordinates(x: 1, y: 2), .relative),
         ])
     }
     
     
-    /// Tests that the `.optimized` property converts `.lineTo` commands to the proper `.axis` command when one of its coordinates is zero, or removes it when it moves to relative (0,0)
+    /// Tests that the `.optimized` property removes a `.moveTo` command whose target is `(0,0)` when its type is `.relative`.
     ///
-    func test_optimized_line() {
+    func test_optimized_moveTo_relativeZeroZero() {
         
         let path = SvgPath(withCommands: [
-            .lineTo(Coordinates(x: 0, y: 0), .absolute),
-            .lineTo(Coordinates(x: 0, y: 0), .relative),
-            .lineTo(Coordinates(x: 0, y: 2), .absolute),
-            .lineTo(Coordinates(x: 0, y: 2), .relative),
-            .lineTo(Coordinates(x: 1, y: 0), .absolute),
-            .lineTo(Coordinates(x: 1, y: 0), .relative),
+            .moveTo(Coordinates(x: 0, y: 0), .relative),
+        ])
+        
+        XCTAssertEqual(path.optimized.commands, [
+        ])
+    }
+    
+    
+    /// Tests that the `.optimized` property does not remove a `.moveTo` command whose target is `(0,0)` when its type is `.absolute`.
+    ///
+    func test_optimized_moveTo_absoluteZeroZero() {
+        
+        let path = SvgPath(withCommands: [
+            .moveTo(Coordinates(x: 0, y: 0), .absolute),
+        ])
+        
+        XCTAssertEqual(path.optimized.commands, [
+            .moveTo(Coordinates(x: 0, y: 0), .absolute),
+        ])
+    }
+    
+    
+    /// Tests that the `.optimized` property does not change a `.lineTo` command with non-zero coordinates.
+    ///
+    func test_optimized_lineTo_nonZero() {
+        
+        let path = SvgPath(withCommands: [
             .lineTo(Coordinates(x: 1, y: 2), .absolute),
             .lineTo(Coordinates(x: 1, y: 2), .relative),
         ])
         
         XCTAssertEqual(path.optimized.commands, [
-            .lineTo(Coordinates(x: 0, y: 0), .absolute),
-            .axis(.vertical, 2, .absolute),
-            .axis(.vertical, 2, .relative),
-            .axis(.horizontal, 1, .absolute),
-            .axis(.horizontal, 1, .relative),
             .lineTo(Coordinates(x: 1, y: 2), .absolute),
             .lineTo(Coordinates(x: 1, y: 2), .relative),
+        ])
+    }
+    
+    
+    /// Tests that the `.optimized` property removes a `.lineTo` command whose target is `(0,0)` when its type is `.relative`.
+    ///
+    func test_optimized_lineTo_relativeZeroZero() {
+        
+        let path = SvgPath(withCommands: [
+            .lineTo(Coordinates(x: 0, y: 0), .relative),
+        ])
+        
+        XCTAssertEqual(path.optimized.commands, [
+        ])
+    }
+    
+    
+    /// Tests that the `.optimized` property does not remove a `.lineTo` command whose target is `(0,0)` when its type is `.absolute`.
+    ///
+    func test_optimized_lineTo_absoluteZeroZero() {
+        
+        let path = SvgPath(withCommands: [
+            .lineTo(Coordinates(x: 0, y: 0), .absolute),
+        ])
+        
+        XCTAssertEqual(path.optimized.commands, [
+            .axis(.vertical, 0, .absolute),
+        ])
+    }
+    
+    
+    /// Tests that the `.optimized` property converts `.lineTo` commands to `.axis` commands of type `.horizontal`  when the `.y` coordinate is zero.
+    ///
+    func test_optimized_lineTo_yZero() {
+        
+        let path = SvgPath(withCommands: [
+            .lineTo(Coordinates(x: 1, y: 0), .absolute),
+            .lineTo(Coordinates(x: 1, y: 0), .relative),
+        ])
+        
+        XCTAssertEqual(path.optimized.commands, [
+            .axis(.horizontal, 1, .absolute),
+            .axis(.horizontal, 1, .relative),
+        ])
+    }
+    
+    
+    /// Tests that the `.optimized` property converts `.lineTo` commands to `.axis` commands of type `.vertical`  when the `.x` coordinate is zero.
+    ///
+    func test_optimized_lineTo_xZero() {
+        
+        let path = SvgPath(withCommands: [
+            .lineTo(Coordinates(x: 0, y: 2), .absolute),
+            .lineTo(Coordinates(x: 0, y: 2), .relative),
+        ])
+        
+        XCTAssertEqual(path.optimized.commands, [
+            .axis(.vertical, 2, .absolute),
+            .axis(.vertical, 2, .relative),
         ])
     }
 }
